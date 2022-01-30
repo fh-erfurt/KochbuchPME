49e9a75dea0969d6ab737568b2be918e75f8948d
valeriidrobiazgho1 <56996014+valeriidrobiazgho1@users.noreply.github.com>
2022-01-30 13:59:35 +0100

    Update InputFragment.java
    Beispiel Rezepte

--- app/src/main/java/com/example/kochbuch/view/ui/input/InputFragment.java
+++ app/src/main/java/com/example/kochbuch/view/ui/input/InputFragment.java
@@ -49,12 +49,122 @@ public class InputFragment extends BaseFragment {
         recIngL.add(kaeseZ);
 
         recipe.setIngredients(recIngL);
-
         viewModel.insertRecipe(recipe);
+
+       //Beispele Rezepte 1
+/*
+        Recipe recipe = new Recipe("Zitronensuppe mit Garnelen",
+                 "Garnelen bei Zimmertemperatur auftauen lassen. Möhren schälen, längs halbieren und in schräge Scheiben schneiden. Zuckerschoten in Streifen schneiden.\n" +
+                        " Knoblauch halbieren und mit Brühe aufkochen. Mit Salz und Pfeffer würzen. Reis zugeben und zugedeckt bei milder Hitze 8 Min. quellen lassen.\n" +
+                        " Möhren zugeben, kurz aufkochen und weitere 8 Min. bei milder Hitze garen. Zuckerschoten und Garnelen zugeben und 2 Min. mitgaren.\n" +
+                        " Eier und Zitronensaft verrühren. Etwas Suppe unter Rühren zu den Eiern geben. Topf von der Herdplatte nehmen, Eimischung einrühren, die Suppe darf nicht mehr kochen! Mit Salz, Pfeffer und Zucker abschmecken.",
+                "leckere Zitronensuppe mit Garnelen", Foodtypes.OMNIVORE);
+
+        RecipeIngredient garnelenZ = new RecipeIngredient(150,new Ingredient("Garnelen",99));
+        RecipeIngredient möhrenZ = new RecipeIngredient(200,new Ingredient("Möhren",35));
+        RecipeIngredient zuckerschotenZ = new RecipeIngredient(100,new Ingredient("Zuckerschoten",80));
+        RecipeIngredient knoblauchZ = new RecipeIngredient(10,new Ingredient("Knoblauch",45));
+        RecipeIngredient gemüsebrüheZ = new RecipeIngredient(800,new Ingredient("Gemüsebrühe",70));
+        RecipeIngredient salzZ = new RecipeIngredient(2,new Ingredient("Salz",1));
+        RecipeIngredient pfefferZ = new RecipeIngredient(2,new Ingredient("Pfeffer",1));
+        RecipeIngredient langkornreisZ = new RecipeIngredient(70,new Ingredient("Langkornreis",100));
+        RecipeIngredient eierZ = new RecipeIngredient(100,new Ingredient("Eier",50));
+        RecipeIngredient zitronensaftZ = new RecipeIngredient(45,new Ingredient("Zitronensaft",30));
+        RecipeIngredient zuckerZ = new RecipeIngredient(2,new Ingredient("Zucker",200));
+
+
+        List<RecipeIngredient> recIngL = new ArrayList<>();
+        recIngL.add(garnelenZ);
+        recIngL.add(möhrenZ);
+        recIngL.add(zuckerschotenZ);
+        recIngL.add(knoblauchZ);
+        recIngL.add(gemüsebrüheZ);
+        recIngL.add(salzZ);
+        recIngL.add(pfefferZ);
+        recIngL.add(langkornreisZ);
+        recIngL.add(eierZ);
+        recIngL.add(zitronensaftZ);
+        recIngL.add(zuckerZ);
+
+        recipe.setIngredients(recIngL);
+
+        viewModel.insertRecipe(recipe); */
+
+        //Beispele Rezepte 2
+
+       /* Recipe recipe = new Recipe("Überbackene Hähnchenbrust",
+                "Zucchini in 1 cm dicke Scheiben schneiden. Paprikaschoten putzen und in grobe Spalten schneiden. Alles in eine Auflaufform geben, mit Salz, Pfeffer und Oregano würzen und mit Olivenöl beträufeln. Ofen auf 220 Grad (Umluft 200 Grad) vorheizen.\n" +
+                         " Mozzarella abtropfen lassen, in 5 Scheiben schneiden und diese halbieren. Hähnchenbrustfilets jeweils in 1cm Abständen fünfmal 1,5–2 cm tief einschneiden. Rundum leicht salzen. Einschnitte mit je 1⁄2 Tl TomatenPesto und 1⁄2 Mozzarella-Scheibe füllen.\n" +
+                         " Hähnchenbrustfilets auf das Gemüse setzen. Im heißen Ofen auf einem Rost auf der untersten Schiene 45 Min. überbacken.",
+                "leckere Überbackene Hähnchenbrust", Foodtypes.OMNIVORE);
+
+        RecipeIngredient zucchiniZ = new RecipeIngredient(360,new Ingredient("Zucchini",38));
+        RecipeIngredient paprikagelbZ = new RecipeIngredient(120,new Ingredient("Paprikagelb",45));
+        RecipeIngredient paprikarotZ = new RecipeIngredient(120,new Ingredient("Paprikarot",45));
+        RecipeIngredient salzZ = new RecipeIngredient(2,new Ingredient("Salz",1));
+        RecipeIngredient pfefferZ = new RecipeIngredient(2,new Ingredient("Pfeffer",1));
+        RecipeIngredient oreganoZ = new RecipeIngredient(10,new Ingredient("Oregano",1));
+        RecipeIngredient olivenölZ = new RecipeIngredient(10,new Ingredient("Olivenöl",180));
+        RecipeIngredient mozzarellaZ = new RecipeIngredient(100,new Ingredient("Mozzarella",210));
+        RecipeIngredient hähnchenbrustfiletsZ = new RecipeIngredient(400,new Ingredient("Hähnchenbrustfilets",165));
+        RecipeIngredient tomatenpestoZ = new RecipeIngredient(70,new Ingredient("Tomatenpesto",90));
+
+
+        List<RecipeIngredient> recIngL = new ArrayList<>();
+        recIngL.add(zucchiniZ);
+        recIngL.add(paprikagelbZ);
+        recIngL.add(paprikarotZ);
+        recIngL.add(salzZ);
+        recIngL.add(pfefferZ);
+        recIngL.add(oreganoZ);
+        recIngL.add(olivenölZ);
+        recIngL.add(mozzarellaZ);
+        recIngL.add(hähnchenbrustfiletsZ);
+        recIngL.add(tomatenpestoZ);
+
+
+        recipe.setIngredients(recIngL);
+
+        viewModel.insertRecipe(recipe); */
+
+        //Beispele Rezepte 3
+/*
+        Recipe recipe = new Recipe("Flanksteak vom Grill mit Asia-Aromen",
+                "Zitronengras am dicken Ende mit einem Plattiereisen oder einem schwerem Messer faserig klopfen. Ingwer und Knoblauch in dünne Scheiben schneiden. Mit Zitronengras, Sojasauce, Süßesauce und Kaffirlimettenblättern zu einer Marinade verrühren.\n" +
+                        " Flanksteak in die Marinade legen und über Nacht abgedeckt im Kühlschrank marinieren.\n" +
+                        " Flanksteak aus der Marinade nehmen und mit Küchenpapier trocken tupfen. Auf dem heißen Grill von jeder Seite 4-5 Minuten grillen, anschließend ca. 5 Minuten ruhen lassen.\n" +
+                        " Das Steak quer zur Faser in 1 cm dicke Scheiben aufschneiden und mit etwas Pfeffer würzen. Dazu passt Rotkohlsalat mit Kokoschips.",
+                "leckere Flanksteak vom Grill mit Asia-Aromen", Foodtypes.OMNIVORE);
+
+        RecipeIngredient zitronengrasZ = new RecipeIngredient(100,new Ingredient("Zitronengras",38));
+        RecipeIngredient ingwerZ = new RecipeIngredient(80,new Ingredient("Ingwer",35));
+        RecipeIngredient knoblauchZ = new RecipeIngredient(40,new Ingredient("Knoblauch",45));
+        RecipeIngredient sojasauceZ = new RecipeIngredient(50,new Ingredient("Sojasauce",19));
+        RecipeIngredient süßesauceZ = new RecipeIngredient(150,new Ingredient("süßesauce",120));
+        RecipeIngredient pfefferZ = new RecipeIngredient(2,new Ingredient("Pfeffer",1));
+        RecipeIngredient kaffirlimettenblätterZ = new RecipeIngredient(10,new Ingredient("Kaffirlimettenblätter",1));
+        RecipeIngredient flanksteakZ = new RecipeIngredient(1000,new Ingredient("Flanksteak",237));
+
+
+
+        List<RecipeIngredient> recIngL = new ArrayList<>();
+        recIngL.add(zitronengrasZ);
+        recIngL.add(ingwerZ);
+        recIngL.add(knoblauchZ);
+        recIngL.add(sojasauceZ);
+        recIngL.add(süßesauceZ);
+        recIngL.add(pfefferZ);
+        recIngL.add(kaffirlimettenblätterZ);
+        recIngL.add(flanksteakZ);
+
+        recipe.setIngredients(recIngL);
+
+        viewModel.insertRecipe(recipe); */
 
 
         return root;
     }
+
 
     //TODO: resume, pause, update, usw...
 }
